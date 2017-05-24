d198f5cfd02104739e98fadbd4800074dc7332b6
Patryk <Patryk@DESKTOP-I3842T9>
2017-05-24 18:11:10 +0200

    Add project files.

---
+++ Fibonacci/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fibonacci.Startup))]
+namespace Fibonacci
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
